79e0bac7f2dc81ccded5be73b4a69f6b0ccff60a
marouaneroussi <mrwanlaroussi@gmail.com>
2020-03-29 00:15:19 +0100

    Updates

---
+++ precache-manifest.c1e35cd305f5ecfa33253729afb7b2e4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4a794c7c9612c983cb8660d5735c3484",
+    "url": "/Covid19/index.html"
+  },
+  {
+    "revision": "628ccce559270246943a",
+    "url": "/Covid19/static/css/2.89f067aa.chunk.css"
+  },
+  {
+    "revision": "c3171c9a5f88749033e2",
+    "url": "/Covid19/static/css/main.6d712676.chunk.css"
+  },
+  {
+    "revision": "628ccce559270246943a",
+    "url": "/Covid19/static/js/2.5929e054.chunk.js"
+  },
+  {
+    "revision": "56729d980da09961afe8d497a21eec12",
+    "url": "/Covid19/static/js/2.5929e054.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c3171c9a5f88749033e2",
+    "url": "/Covid19/static/js/main.83d2b7c7.chunk.js"
+  },
+  {
+    "revision": "f6440b93d05e310f8d94",
+    "url": "/Covid19/static/js/runtime-main.e7a6fa3b.js"
+  }
+]);
